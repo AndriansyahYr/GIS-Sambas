02326d6ae4a006bb88c73adfd214d1b4f81ca384
Andriansyah <andriansyahgl@gmail.com>
2017-02-18 09:56:35 +0700

    GIS

---
+++ data/json_letak5.js
@@ -0,0 +1,29 @@
+var json_letak5={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Nama": "Segarau Parit" }, "geometry": { "type": "Point", "coordinates": [ 109.12583906611468, 1.263124857901507 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Nama": "Sempalai" }, "geometry": { "type": "Point", "coordinates": [ 109.17062026740875, 1.268021606990907 ] } },
+{ "type": "Feature", "properties": { "id": "4", "Nama": "Bekut" }, "geometry": { "type": "Point", "coordinates": [ 109.17878350722798, 1.248434555410814 ] } },
+{ "type": "Feature", "properties": { "id": "5", "Nama": "Mak Rampai" }, "geometry": { "type": "Point", "coordinates": [ 109.17131997367896, 1.235376439813231 ] } },
+{ "type": "Feature", "properties": { "id": "7", "Nama": "Seberkat" }, "geometry": { "type": "Point", "coordinates": [ 109.22211581082226, 1.204001242597172 ] } },
+{ "type": "Feature", "properties": { "id": "9", "Nama": "Mekar Sekuntum" }, "geometry": { "type": "Point", "coordinates": [ 109.14380918903751, 1.20671423793896 ] } },
+{ "type": "Feature", "properties": { "id": "10", "Nama": "Mensere" }, "geometry": { "type": "Point", "coordinates": [ 109.126752301124, 1.208264519776889 ] } },
+{ "type": "Feature", "properties": { "id": "11", "Nama": "Pusaka" }, "geometry": { "type": "Point", "coordinates": [ 109.11357197864538, 1.203226100574785 ] } },
+{ "type": "Feature", "properties": { "id": "12", "Nama": "Segedong" }, "geometry": { "type": "Point", "coordinates": [ 109.10271759542769, 1.1888859335811 ] } },
+{ "type": "Feature", "properties": { "id": "13", "Nama": "Sungai Kelambu" }, "geometry": { "type": "Point", "coordinates": [ 109.13062886655889, 1.183847478800497 ] } },
+{ "type": "Feature", "properties": { "id": "14", "Nama": "Serumpun Buluh" }, "geometry": { "type": "Point", "coordinates": [ 109.14613512829844, 1.185397773556146 ] } },
+{ "type": "Feature", "properties": { "id": "15", "Nama": "Dungun Prapakan" }, "geometry": { "type": "Point", "coordinates": [ 109.18053220409594, 1.185873335890181 ] } },
+{ "type": "Feature", "properties": { "id": "16", "Nama": "Pangkalan Kongsi" }, "geometry": { "type": "Point", "coordinates": [ 109.16490483020878, 1.17375671727627 ] } },
+{ "type": "Feature", "properties": { "id": "17", "Nama": "Batu Makjage" }, "geometry": { "type": "Point", "coordinates": [ 109.18595394605681, 1.152393077958615 ] } },
+{ "type": "Feature", "properties": { "id": "18", "Nama": "Bukit Segoler" }, "geometry": { "type": "Point", "coordinates": [ 109.17957542610286, 1.115086041765368 ] } },
+{ "type": "Feature", "properties": { "id": "19", "Nama": "Serindang" }, "geometry": { "type": "Point", "coordinates": [ 109.14258001036994, 1.132623597268981 ] } },
+{ "type": "Feature", "properties": { "id": "20", "Nama": "Matang Labong" }, "geometry": { "type": "Point", "coordinates": [ 109.13205545244593, 1.143146079734536 ] } },
+{ "type": "Feature", "properties": { "id": "21", "Nama": "Mak Tangguk" }, "geometry": { "type": "Point", "coordinates": [ 109.12153089452192, 1.153668523639034 ] } },
+{ "type": "Feature", "properties": { "id": "22", "Nama": "Maribas" }, "geometry": { "type": "Point", "coordinates": [ 109.29438878527395, 1.10456345820475 ] } },
+{ "type": "Feature", "properties": { "id": "23", "Nama": "Seret Ayon" }, "geometry": { "type": "Point", "coordinates": [ 109.36008754079965, 1.037919578369393 ] } },
+{ "type": "Feature", "properties": { "id": "8", "Nama": "Tebas Kuala" }, "geometry": { "type": "Point", "coordinates": [ 109.15505290546199, 1.229420048756297 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Nama": "Sejiram" }, "geometry": { "type": "Point", "coordinates": [ 109.1818353778831, 1.222879263464005 ] } },
+{ "type": "Feature", "properties": { "id": "6", "Nama": "Tebas Sungai" }, "geometry": { "type": "Point", "coordinates": [ 109.18316427918646, 1.206220629128426 ] } }
+]
+}
